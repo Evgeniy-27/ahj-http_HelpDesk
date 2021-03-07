b1bb23a23e634c19748e092738ca604ef59db340
Evgeniy-27 <Li.Evgen@yandex.ru>
2021-03-07 23:44:36 +0700

    Отмена изменений

--- server.js
+++ server.js
@@ -34,38 +34,7 @@ app.use(
       json: true,
     })
   );
-  app.use(async (ctx, next) => {
-    const origin = ctx.request.get('Origin');
   
-    if (!origin) {
-      return next();
-    }
-  
-    const headers = { 'Access-Control-Allow-Origin': '*' };
-  
-    if (ctx.request.method !== 'OPTIONS') {
-      ctx.response.set({ ...headers });
-      try {
-        return next();
-      } catch (e) {
-        e.headers = { ...e.headers, ...headers };
-        throw e;
-      }
-    }
-  
-    if (ctx.request.get('Access-Control-Request-Method')) {
-      ctx.response.set({
-        ...headers,
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-      });
-      if (ctx.request.get('Access-Control-Request-Headers')) {
-        ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-      }
-      ctx.response.status = 204;
-    }
-  
-    return null;
-  });
 app.use(async ctx => {
     ctx.response.set({
         'Access-Control-Allow-Origin': '*',
